ab45317d3718e9499c54928e824581c107954194
berserkkv <4121nnn@gmail.com>
2025-05-11 20:24:57 +0300

    push

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import.meta.env.VITE_API_BASE_URL
 
-//const API = "http://193.180.208.245:8080/api/users";
-const API = "http://localhost:8080/api/users";
+const API = "http://" +  import.meta.env.VITE_API_URL  + ":8080/api/users";
+//const API = "http://localhost:8080/api/users";
 
 export const getUsers = () => axios.get(API);
 export const getUser = (id) => axios.get(`${API}/${id}`);
